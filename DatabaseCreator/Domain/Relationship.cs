77347c8b4abc4cc4b842a5db0ceda39f3dd76ad9
Safonov Anatoli <hartigans@live.com>
2014-04-22 15:51:00 +0300

    edit Domain project

--- DatabaseCreator/Domain/Relationship.cs
+++ DatabaseCreator/Domain/Relationship.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace Domain
 {
 	public class Relationship : BaseClass
 	{
+		[MaxLength(128)]
+		public string Name { get; set; }
 		public Type ParentType { get; set; }
 
 		public Type ChildType { get; set; }
